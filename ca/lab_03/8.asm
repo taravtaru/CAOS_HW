0536f41eb77c54b9418f9629f9b46f46d5bc3d43
Миняйцев Иван Василевич <111189316+taravtaru@users.noreply.github.com>
2025-01-22 15:15:25 +0300

    FIX 8.asm

--- ca/lab_03/8.asm
+++ ca/lab_03/8.asm
@@ -5,16 +5,18 @@ mv t0, a0
 li a7, 5
 ecall
 mv t1, a0
-mv s1, t1
+
+li s0, 10
 
 addi t0, t0, 5
-addi t1, t1, 10
-addi s1, s1, -1
-
 div t0, t0, t1
-div t1, t0, s1
 
-mv a0, t1
+addi t1, t1, -1
+div t1, s0, t1
+
+add t0, t0, t1
+
+mv a0, t0
 li a7, 1
 ecall
 
